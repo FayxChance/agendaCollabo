2da477bfe8c3d02280f4b56d809a12fc0d35626e
Traore Bakary <bakary12traore@yahoo.fr>
2019-01-22 15:03:17 +0000

    Version recente

--- view/form/ajoutEvent.php
+++ view/form/ajoutEvent.php
@@ -7,13 +7,45 @@
  <td><label>Nom </label> <input type="text" name="nomEvent" placeholder="Nom"  /></td>
  </tr>
  <tr>
- <td><label>Heure debut </label> <input type="time" name="HoraireDebut" /></td>
+ <td><label>Heure debut</label><select name="Heure debut">
+ <?php
+ 
+ for ( $i=0; $i<24; $i++){
+  echo "<option value='$i'>"; echo $i; echo"</option>";
+  }
+ ?>
+ 
+</select></td>
+<td><label>minute debut</label><select name="Heure debut">
+ <?php
+ 
+ for ( $i=0; $i<60; $i=$i+5){
+  echo "<option value='$i'>"; echo $i; echo"</option>";
+  }
+ ?>
+ 
+</select></td>
+<td><label>Heure fin</label><select name="Heure debut">
+ <?php
+ 
+ for ( $i=0; $i<24; $i++){
+  echo "<option value='$i'>"; echo $i; echo"</option>";
+  }
+ ?>
+ 
+</select></td>
+<td><label>minute fin</label><select name="Heure debut">
+ <?php
+ 
+ for ( $i=0; $i<60; $i=$i+5){
+  echo "<option value='$i'>"; echo $i; echo"</option>";
+  }
+ ?>
+</select></td>
  </tr>
  <tr>
- <td><label>Heure Fin </label><input type="time" name="HaraireFin" /></td>
- </tr>
- <tr>
- <td><label>Description </label><input type="text" name="Description" placeholder="Description"/></td>
+ <td><label>Description </label> <textarea name="Description" rows="5" cols="40"></textarea>
+    
  </tr>
  <tr>
 <td> <label>Lieu </label><input type="text" name="Lieu" placeholder="Lieu"/></td>
