120ce01769c66a3be0f2b35ff4b8692c858a9850
Traore Bakary <bakary12traore@yahoo.fr>
2019-01-22 14:09:26 +0000

    ajout

--- view/form/ajoutEvent.php
+++ view/form/ajoutEvent.php
@@ -1,11 +1,25 @@
-
-<form method="post" action="">
 <h1>Ajout d'un Evenement </h1>
- <input type="text" name="nomEvent"   />
- <input type="time" name="HoraireDebut"/>
- <input type="time" name="HaraireFin"/>
- <input type="text" name="Description"/>
- <input type="text" name="Lieu"/>
- <input type="submit" name="Envoyer" value="Envoyer"/>
- 
+<form method="post" action="">
+
+<fieldset>
+<table>
+<tr>
+ <td><label>Nom </label> <input type="text" name="nomEvent" placeholder="Nom"  /></td>
+ </tr>
+ <tr>
+ <td><label>Heure debut </label> <input type="time" name="HoraireDebut" /></td>
+ </tr>
+ <tr>
+ <td><label>Heure Fin </label><input type="time" name="HaraireFin" /></td>
+ </tr>
+ <tr>
+ <td><label>Description </label><input type="text" name="Description" placeholder="Description"/></td>
+ </tr>
+ <tr>
+<td> <label>Lieu </label><input type="text" name="Lieu" placeholder="Lieu"/></td>
+ </tr>
+ <tr><td><input type="submit" name="Envoyer" value="Envoyer"/></td></tr>
+ </table>
+ </fieldset>
+
  </form>
