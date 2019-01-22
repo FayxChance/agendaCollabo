119daf5d120a0366dea58328cee9ae612020b4dd
Traore Bakary <bakary12traore@yahoo.fr>
2019-01-22 13:42:41 +0000

    Ajout d'evenement

---
+++ view/form/ajoutEvent.php
@@ -0,0 +1,11 @@
+
+<form method="post" action="">
+<h1>Ajout d'un Evenement </h1>
+ <input type="text" name="nomEvent"   />
+ <input type="time" name="HoraireDebut"/>
+ <input type="time" name="HaraireFin"/>
+ <input type="text" name="Description"/>
+ <input type="text" name="Lieu"/>
+ <input type="submit" name="Envoyer" value="Envoyer"/>
+ 
+ </form>
