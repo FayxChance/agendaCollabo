6be89d37b66b020c5ae02171888c8b2b5edda771
NepNep <fayxchancetom@outlook.com>
2019-01-31 10:40:16 +0100

    test form bootstrap et db localhsot

---
+++ model/db.php
@@ -0,0 +1,6 @@
+<?php
+  mysqli_connect("localhost", "root", "root","l2_gr5");
+  // mysqli_set_charset($c, "utf8");
+  echo mysqli_connect_error();
+
+?>
--- test.php
+++ test.php
@@ -7,10 +7,24 @@
     }
   </style>
   <main>
-    <div class="container" style="background-color:skyblue">
-      <h1>Fixed Container</h1>
+    <!--<div class="jumbotron jumbotron-fluid bg-info text-white text-center">
+      <div class="container" >
+        <p class="display-4">Fixed Container</p>
+        <p class="lead" >Look at this </p>
+      </div>
+    </div>-->
+    <div class="col-2 container bg-info row ">
+      <form class="form-group" action="test.php" method="post">
+        <input type="text" name="pseudoLog" class="form-control" value="" placeholder="Pseudo">
+        <input type="text" name="mdpLog" class="form-control" value="" placeholder="Password">
+      </form>
+      <form class="form-group" action="test.php" method="post">
+        <input type="text" name="pseudoSign" class="form-control" value="" placeholder="Pseudo">
+        <input type="text" name="mdpSign" class="form-control" value="" placeholder="Password">
+      </form>
     </div>
-    <div class="container-fluid" style="background-color:skyblue">
+    <!--
+    <div class="container-fluid">
       <div class="row" style="background:red">
         <div class="col-8"style="background:yellow">
           col-xl-8
@@ -19,8 +33,8 @@
           col-xl-4
         </div>
       </div>
-      <div class="row justify-content-md-center" style="background:aquamarine">
-        <div class="col-md-4"style="background:">
+      <div class="row justify-content-md-center" >
+        <div class="col-md-4">
           col-xl-8
         </div>
         <div class="col-md-4">
@@ -44,6 +58,6 @@
       <label for="title">title</label>
       <input type="text" name="champ" value="" placeholder="E-mail">
       <input type="submit" name="submit" value="Envoyez">
-    </form>
+    </form>-->
   </main>
 <?php include_once("./view/layouts/footer.php"); ?>
