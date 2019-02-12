e13c536cb2400f05a0b22214590c79d9c6fa6a67
Toinou <chatel23081999@gmail.com>
2019-02-12 15:41:43 +0100

    Maj Functions
    actionbdd() terminé

--- model/functions.php
+++ model/functions.php
@@ -10,12 +10,12 @@
   function actionbdd($requete,$nomTab, $data, $condition){
     //Insert les elements de $data dans la bd renseignée
     //__________________GUIDE UTILISATION__________________
-    //$requete : DELETE / UPDATE / INSERT / SELECT
+    //$requete : DELETE / INSERT / SELECT
     //$nomTab : Nom de la table concernée
     //$data : les informations que vous voulez insérer (par exemple) sous forme de tableau
     //$condition : condition de suppression (si aucune condition nécéssaire, renseigner 0)
     //EXEMPLE :
-    // actionbdd(INSERT,"utilisateur", ["Nom"=>"'Mabite'"],0) ;
+    // actionbdd(INSERT,"utilisateur", ["Nom"=>"'Fesse'"],0) ;
     // actionbdd("DELETE","utilisateur", [],"id=2") ; Supprime dans la table utilisateur la ligne dont l'id est 2
     // actionbdd("SELECT","Event", ["Nom"=>"''"],"id=3")
     global $c;
