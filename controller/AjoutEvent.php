885822fd70895b2f4a037e6b981256366a9d107e
Osmium <chatel2308@gmail.com>
2019-02-18 20:23:48 +0100

    Controller Ajout Event à tester

--- controller/AjoutEvent.php
+++ controller/AjoutEvent.php
@@ -1,12 +1,10 @@
 <?php include_once "../model/functions.php"
   session_start();
-  actionbdd("INSERT","Event",
-  ["NomEvent"=>"'".$_POST['nomEvent']."'"])
 
-  $_POST['nomEvent']
-  $_POST['dateEventDebut']
-  $_POST['dateEventFin']
-  $_POST['description']
-  $_POST['submitAjoutEvent']
-
+  actionbdd(INSERT,"Event", [
+    "idEvent"=>"''",
+    "nomEvent"=>"'.$_POST['nomEvent'].'",
+    "Debut"=>"'.$_POST['dateEventDebut'].'",
+    "Fin"=>"'.$_POST['dateEventFin'].'",
+    "Desc"=>"'.$_POST['description'].'"],0);
  ?>
