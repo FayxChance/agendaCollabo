0bbdf3382c1dc10178cd7e31f366af3d1262212e
NepNep <fayxchancetom@outlook.com>
2019-02-19 00:09:19 +0100

    BootStrap test affichage

--- index.php
+++ index.php
@@ -1,8 +1,13 @@
 <?php
+include_once "./view/layouts/header.php";
 session_start();
 if($_SESSION['connecte']){
   echo "Bienvenue ". $_SESSION['pseudo'];
   include_once "./view/forms/logout.php";
+  include_once "./model/classes/Semaine.php";
+  $maSemaine=new Semaine(date("Y-m-d"));
+  $maSemaine->AfficheEvent(0,0);
+  $date = date('m/d/Y h:i:s a', time());
 }
 else {
   include_once "./view/forms/formLogin.php";
--- model/classes/Semaine.php
+++ model/classes/Semaine.php
@@ -19,12 +19,30 @@ class Semaine{
 		$event=actionbdd("SELECT","Event",["NomEvent" =>"''"], " DebutEvent LIKE '%".$chaine."%'");
 		return  !is_null($event);
 	}
-	// public AfficheEvent($JourDebut){
-	//
-	//
-	//
-	//
-	// }
+	public function AfficheEvent($JourDebut,$event,$idUtilisateur){
+		echo "
+		<div class='container-fluid'>
+			<div class='row'>
+
+		";
+		for ($i=0; $i < 7; $i++) {
+			$date=date("Y-m-d",strtotime("+".$i." days"));
+			echo "
+				<div class='col-1'>
+				".$date;
+				//$requete=actionbdd("SELECT","event",["NomEvent"=>"''"],"Utilisateur=$idUtilisateur AND DebutEvent LIKE '%".$date."%'");
+				// while ($row=mysqli_fetch_assoc($requete)) {
+				// 	echo $row['NomEvent'];
+				// }
+				echo "
+				</div>
+			";
+		}
+		echo "
+			</div>
+		</div>";
+
+	}
 
 }
 
