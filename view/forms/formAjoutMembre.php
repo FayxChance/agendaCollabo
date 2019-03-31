f36f60044728a033c9e8a0d4e078f3250103e7a7
Osmium <chatel2308@gmail.com>
2019-04-01 00:04:09 +0200

    Form Ajout Membre v0.001

--- view/forms/formAjoutMembre.php
+++ view/forms/formAjoutMembre.php
@@ -1,1 +1,29 @@
-<?php  ?>
+<?php session_start(); ?>
+
+<div class="row">
+  <div class="formAction" id="formAjoutMembre">
+    <h3 class="text-center"> Ajout d'un Evenement </h3>
+    <form class="well" method="post" action="./controller/AjoutMembre.php">
+      <div class="form-group">
+        <label for="nom"> Nom </label>
+        <input  class="form-control" type="text" name="nomNew" placeholder="Nom"  />
+      </div>
+      <div class="form-group">
+        <label for="dateDebut"> Date début </label>
+        <input type="datetime-local" name="dateEventDebut" value="">
+      </div>
+      <div class="form-group">
+        <label for="dateFin"> Date fin </label>
+        <input type="datetime-local" name="dateEventFin" value="">
+      </div>
+      <div class="form-group">
+        <label for="description">Description</label>
+        <textarea class="form-control" name="description" placeholder="Entrer une description (optionnel)"></textarea>
+      </div>
+      <div class="form-group">
+        <input class="btn btn-default" type="submit" name="submitAjoutEvent" value="Ajouter">
+      </div>
+    </form>
+  </div>
+
+</div>
