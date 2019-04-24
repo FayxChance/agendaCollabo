c89d7f59d8ec3a2b1b2363fb45372e676b1ce5e0
FayxChance <fayxchancetom@yahoo.fr>
2019-04-24 10:45:00 +0200

    Modif index et structure page

--- controller/dynamique.php
+++ controller/dynamique.php
@@ -1,10 +1,16 @@
 <?php
-  switch ($_GET['action']) {
-    case 'agenda':
-      include_once "./view/layouts/agenda.php";
-      break;
+switch ($_GET['action']) {
+    case 'home':
+        include_once "./view/home.php";
+        break;
+    case 'event':
+        include_once "./view/event.php";
+        break;
+    case 'group':
+        include_once "./view/groupes.php";
+        break;
+    case 'notification'
     default:
-      include_once "./view/layouts/login.php";
-      break;
-  }
- ?>
+        include_once "./view/home.php";
+        break;
+}
